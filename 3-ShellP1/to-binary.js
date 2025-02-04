be6f56c15bf68590a07f2584c6b0b5a4a9cd0a25
Alex Villablanca <alex@villablanca.tech>
2025-02-04 10:39:52 -0500

    HW3: Extra credit dragon command

---
+++ 3-ShellP1/to-binary.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env bun
+
+import { platform } from "os";
+
+const bytes = await Bun.file("./dragon").bytes();
+
+let out = "const unsigned char DRAGON[] = {\n";
+const bytesPerLine = 16;
+for (let i = 0; i < bytes.length; i++) {
+  // Insert an indent at the start of each line.
+  if (i % bytesPerLine === 0) {
+    out += "    ";
+  }
+  // Format each byte as 0x??
+  out += "0x" + bytes[i].toString(16).padStart(2, "0");
+
+  // Add a comma except for the final element.
+  if (i !== bytes.length - 1) {
+    out += ", ";
+  }
+
+  // Insert a newline after the designated number of bytes.
+  if ((i + 1) % bytesPerLine === 0) {
+    out += "\n";
+  }
+}
+// Make sure we end with a newline if the last line wasn't complete.
+if (bytes.length % bytesPerLine !== 0) {
+  out += "\n";
+}
+out += "};\n";
+
+switch (platform()) {
+  case "darwin": {
+    const proc = Bun.spawn(["pbcopy"], {
+      stdin: "pipe"
+    });
+    proc.stdin.write(out);
+    proc.stdin.end();
+    await proc.exited;
+    break;
+  }
+}
+
+console.log("C array copied to clipboard!");
+console.log(out);
